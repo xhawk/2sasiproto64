cf919d42addea1274fa3b3b778a0764e8324e15c
Tomi Savolainen <tomi.savolainen@maanmittauslaitos.fi>
2015-10-21 13:14:00 +0300

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,15 @@
 #
 # Setup the the box. This runs as root
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
 
 apt-get -y install curl
 apt-get -y install wget
 apt-get install vim -y
 apt-get install zip -y
 
-apt-get install nodejs
-sudo apt-get install build-essential
+apt-get install nodejs -y
+sudo apt-get install build-essential -y
 
 apt-get install openjdk-7-jdk -y
 
@@ -18,6 +18,6 @@ wget https://services.gradle.org/distributions/gradle-2.7-bin.zip -P /etc
 unzip /etc/gradle-2.7-bin.zip -d /etc
 mv /etc/gradle-2.7 /etc/gradle
 
-echo "/etc/gradle/bin:" + $PATH | sudo tee /etc/environment
+echo "/etc/gradle/bin:"$PATH | sudo tee /etc/environment
 
 # You can install anything you need here.
